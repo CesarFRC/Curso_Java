b6fb8ef284d8525acbd8d4b5323a65b99d849e06
LapOne MX <LapOne MX@DESKTOP-1UM34DG>
2024-02-27 21:40:13 -0600

    Solo se agrego a Sourge package un nuevo ini01

---
+++ src/java/ini/ini01.java
@@ -0,0 +1,12 @@
+package ini;
+
+import java.io.Serializable;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+
+public class ini01 implements Serializable {
+    
+}
